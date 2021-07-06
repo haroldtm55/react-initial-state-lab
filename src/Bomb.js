d3ddfbf2edd12c4bfc5185557f49e729ec72cabe
Harold Torres Marino <haroldtm55@gmail.com>
2021-07-06 18:25:20 +1000

    Lab completed

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
+import React, { Component } from 'react';
+
 // your Bomb code here!
+class Bomb extends  Component {
+  constructor(props) {
+    super()
+    console.log(props)
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  
+  render() {
+    return (
+      this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+    )
+  }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,21 @@
 // your ImageSlider code here!
+
+import { Component } from "react";
+
+class ImageSlider extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+  
+  
+  render() {
+    return (
+      `I am on slide ${this.state.currentSlideIndex}`
+      )
+  }
+}
+
+export default ImageSlider
